39108bbd2cb847c5cf785f155b034a2c640fd5aa
dyd3378 <dyd3378@gmail.com>
2022-01-05 18:51:49 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql');
 
+// mysql connection
 const connection = mysql.createConnection({
     host: conf.host,
     user: conf.user,
@@ -17,6 +18,10 @@ const connection = mysql.createConnection({
     database: conf.database
 });
 connection.connect();
+
+//파일 업로드
+const multer = require('multer');
+const upload = multer({dest: './upload'})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -28,6 +33,23 @@ app.get('/api/customers',(req,res) =>{
             res.send(rows);
         }
     );
+});
+
+app.use('/image',express.static('./upload'));
+app.post('/api/customers',upload.single('image'),(req, res)=>{
+    let sql  = 'INSERT INTO management.CUSTOMER VALUES (null, ?, ? , ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+
+    let params = [image, name, birthday, gender, job];
+    connection.query(sql, params,
+        (err, rows, fields)=>{
+            res.send(rows);
+        }
+    ); 
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
